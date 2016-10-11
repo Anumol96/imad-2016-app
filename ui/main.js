73f471cd46b8fdaa645f470f775877bf8be0fd87
Anumol96 <anumolcs1996@gmail.com>
2016-10-12 01:09:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,7 +10,7 @@ function moveRight (){
     img.style.marginLeft=marginLeft + 'px';
 }
 
-window.onload = function(){
+window.onclick = function(){
     var interval=setInterval(moveRight, 50);
 
 };
