2798ee6cb908f38eddf2f1f2751eb354305398c5
Anumol96 <anumolcs1996@gmail.com>
2016-10-12 00:56:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,14 @@ console.log('Loaded!');
 
 var element=document.getElementById('main-text');
 element.innerHTML='newvalue';
+
+var img=document.getElementById('modi');
+var marginLeft=0;
+function moveRight (){
+    marginLeft=marginLeft+2;
+    img.style.marginLeft=marginLeft + 'px';
+}
+
+img.onclick= function(){
+    var interval=setInterval(moveRight,50)
+};
